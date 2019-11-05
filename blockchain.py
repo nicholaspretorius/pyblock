1b7acc5f6e263777c15cffc466e736fb1ae1370d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-11-05 13:06:50 +0200

    fix: Correct bug in flow

--- blockchain.py
+++ blockchain.py
@@ -14,21 +14,37 @@ def add_value(value, last=[ORIGIN]):
     print(blockchain)
 
 
-def get_input():
+def get_transaction_amount():
     return float(input('Please enter your transaction amount: '))
 
 
-transaction = get_input()
+def get_user_choice():
+    choice = input('''What do you want to do?: 
+        1: Enter transaction amount.
+        2: Print blockchain.
+        ''')
+    return choice
+
+
+def print_blockchain():
+    for block in blockchain:
+        print('Block: ')
+        print(block)
+
+
+transaction = get_transaction_amount()
 add_value(transaction)
-transaction = get_input()
-add_value(transaction, get_last_blockchain_value())
-transaction = get_input()
-add_value(transaction, get_last_blockchain_value())
 
 
-for block in blockchain:
-    print('Block: ')
-    print(block)
+while True:
+    choice = get_user_choice()
+
+    if choice == '1':
+        transaction = get_transaction_amount()
+        add_value(transaction, get_last_blockchain_value())
+    else:
+        print_blockchain()
+        False
 
 
 print('Done!!!')
